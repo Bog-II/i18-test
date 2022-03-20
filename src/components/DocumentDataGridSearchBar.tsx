97835280a6bf7ea95374560062a5e8b9ed753988
Bogdan <bogsacha@gmail.com>
2022-03-20 17:25:59 +0100

    add onClick onChange for document search bar

--- src/components/DocumentDataGridSearchBar.tsx
+++ src/components/DocumentDataGridSearchBar.tsx
@@ -1,13 +1,25 @@
 import { Clear, Search } from '@mui/icons-material';
 import { IconButton, InputAdornment, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const DocumentDataGridSearchBar = () => {
+  const [searchBarValue, setSearchBarValue] = useState('');
+
+  const onValueClear = () => {
+    setSearchBarValue('');
+  };
+
+  const onValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchBarValue(event.target.value);
+  };
+
   return (
     <TextField
       id="standard-basic"
       fullWidth
       label="Recherche"
+      value={searchBarValue}
+      onChange={onValueChange}
       InputProps={{
         startAdornment: (
           <InputAdornment position="start">
@@ -16,7 +28,7 @@ export const DocumentDataGridSearchBar = () => {
         ),
         endAdornment: (
           <InputAdornment position="end">
-            <IconButton>
+            <IconButton onClick={onValueClear}>
               <Clear />
             </IconButton>
           </InputAdornment>
